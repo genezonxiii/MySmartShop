591a949f5d449a7337f4a2397480d7705db6a35d
Melvin Chu <genezonxiii@msn.com>
2019-12-16 11:25:55 +0800

    Main
    1. photo

--- app/Main.js
+++ app/Main.js
@@ -4,14 +4,10 @@ import { View, ScrollView, Image, AsyncStorage } from 'react-native'
 import Menu from './component/Menu'
 
 import { mains } from './component/theme'
-import { GOOGLE_DRIVE, HOST_SERVER } from './component/constants';
 
 class Main extends Component {
 	constructor(props){
 		super(props)
-		this.state = {
-			uri: GOOGLE_DRIVE + '1YmiMsYPbNgVHnhcW1N0Kt4l6k4_12HDv',
-		}
 	}
 
 	async componentDidMount() {
@@ -24,12 +20,11 @@ class Main extends Component {
 	}
 
 	render () {
-		const { uri } = this.state
 		return(
 			<View style={mains.container}>
 				<View style={mains.topContainer}>
 		          <Image 
-		            source={require('./image/190514_0002.jpg')}
+		            source={require('./image/IMG_0010.jpg')}
 		            style={mains.image} />
 				</View>
 				<View style={mains.bottomContainer}>
