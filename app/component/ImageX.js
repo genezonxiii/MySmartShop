9f791130f460686a6eec3826bd7d2e4ef9c0a4d6
Melvin Chu <genezonxiii@msn.com>
2020-01-09 11:46:28 +0800

    Location Guide
    1. Update photo

--- app/component/ImageX.js
+++ app/component/ImageX.js
@@ -6,52 +6,17 @@ const ImageX = ({ value }) => (
 	<View>
 	
     	{
-			value == '1A'? <Image style={imageX.image} source={ require('../image/p/position-P4-1A.png') } />: 
-			value == '1B'? <Image style={imageX.image} source={ require('../image/p/position-P4-1B.png') } />: 
-			value == '1C'? <Image style={imageX.image} source={ require('../image/p/position-P4-1C.png') } />: 
-			value == '1D'? <Image style={imageX.image} source={ require('../image/p/position-P4-1D.png') } />: 
-			value == '1E'? <Image style={imageX.image} source={ require('../image/p/position-P4-1E.png') } />: 
-			value == '1F'? <Image style={imageX.image} source={ require('../image/p/position-P4-1F.png') } />: 
-			value == '1G'? <Image style={imageX.image} source={ require('../image/p/position-P4-1G.png') } />: 
-			value == '1H'? <Image style={imageX.image} source={ require('../image/p/position-P4-1H.png') } />: 
-			value == '1I'? <Image style={imageX.image} source={ require('../image/p/position-P4-1I.png') } />: 
-			value == '1J'? <Image style={imageX.image} source={ require('../image/p/position-P4-1J.png') } />: 
-			value == '1K'? <Image style={imageX.image} source={ require('../image/p/position-P4-1K.png') } />: 
-			value == '1L'? <Image style={imageX.image} source={ require('../image/p/position-P4-1L.png') } />: 
-			value == '1M'? <Image style={imageX.image} source={ require('../image/p/position-P4-1M.png') } />: 
-			value == '1N'? <Image style={imageX.image} source={ require('../image/p/position-P4-1N.png') } />: 
-			value == '1O'? <Image style={imageX.image} source={ require('../image/p/position-P4-1O.png') } />: 
-			value == '1P'? <Image style={imageX.image} source={ require('../image/p/position-P4-1P.png') } />: 
-			value == '1Q'? <Image style={imageX.image} source={ require('../image/p/position-P4-1Q.png') } />: 
-			value == '1R'? <Image style={imageX.image} source={ require('../image/p/position-P4-1R.png') } />: 
-			value == '2A'? <Image style={imageX.image} source={ require('../image/p/position-P5-2A.png') } />: 
-			value == '2B'? <Image style={imageX.image} source={ require('../image/p/position-P5-2B.png') } />: 
-			value == '2C'? <Image style={imageX.image} source={ require('../image/p/position-P5-2C.png') } />: 
-			value == '2D'? <Image style={imageX.image} source={ require('../image/p/position-P5-2D.png') } />: 
-			value == '2E'? <Image style={imageX.image} source={ require('../image/p/position-P5-2E.png') } />: 
-			value == '2F'? <Image style={imageX.image} source={ require('../image/p/position-P5-2F.png') } />: 
-			value == '2G'? <Image style={imageX.image} source={ require('../image/p/position-P5-2G.png') } />: 
-			value == '2H'? <Image style={imageX.image} source={ require('../image/p/position-P5-2H.png') } />: 
-			value == '2I'? <Image style={imageX.image} source={ require('../image/p/position-P5-2I.png') } />: 
-			value == '2J'? <Image style={imageX.image} source={ require('../image/p/position-P5-2J.png') } />: 
-			value == '2K'? <Image style={imageX.image} source={ require('../image/p/position-P5-2K.png') } />: 
-			value == '2L'? <Image style={imageX.image} source={ require('../image/p/position-P5-2L.png') } />: 
-			value == '2M'? <Image style={imageX.image} source={ require('../image/p/position-P5-2M.png') } />: 
-			value == '2N'? <Image style={imageX.image} source={ require('../image/p/position-P5-2N.png') } />: 
-			value == '2O'? <Image style={imageX.image} source={ require('../image/p/position-P5-2O.png') } />: 
-			value == '2P'? <Image style={imageX.image} source={ require('../image/p/position-P5-2P.png') } />: 
-			value == '2Q'? <Image style={imageX.image} source={ require('../image/p/position-P5-2Q.png') } />: 
-			value == '2R'? <Image style={imageX.image} source={ require('../image/p/position-P5-2R.png') } />: 
-			value == '2S'? <Image style={imageX.image} source={ require('../image/p/position-P5-2S.png') } />: 
-			value == '2T'? <Image style={imageX.image} source={ require('../image/p/position-P5-2T.png') } />: 
-			value == '6A'? <Image style={imageX.image} source={ require('../image/p/position-P6-6A.png') } />: 
-			value == '6B'? <Image style={imageX.image} source={ require('../image/p/position-P6-6B.png') } />: 
-			value == '6C'? <Image style={imageX.image} source={ require('../image/p/position-P6-6C.png') } />: 
-			value == '6D'? <Image style={imageX.image} source={ require('../image/p/position-P6-6D.png') } />: 
-			value == '6E'? <Image style={imageX.image} source={ require('../image/p/position-P6-6E.png') } />: 
-			value == '6F'? <Image style={imageX.image} source={ require('../image/p/position-P6-6F.png') } />: 
-			value == '6G'? <Image style={imageX.image} source={ require('../image/p/position-P6-6G.png') } />: 
-			value == '6H'? <Image style={imageX.image} source={ require('../image/p/position-P6-6H.png') } />: 
+			value == 'A01'? <Image style={imageX.image} source={ require('../image/p/A01.png') } />: 
+			value == 'A02'? <Image style={imageX.image} source={ require('../image/p/A02.png') } />: 
+			value == 'B01'? <Image style={imageX.image} source={ require('../image/p/B01.png') } />: 
+			value == 'B02'? <Image style={imageX.image} source={ require('../image/p/B02.png') } />: 
+			value == 'C'? <Image style={imageX.image} source={ require('../image/p/C.png') } />: 
+			value == 'D'? <Image style={imageX.image} source={ require('../image/p/D.png') } />: 
+			value == 'E'? <Image style={imageX.image} source={ require('../image/p/E.png') } />: 
+			value == 'F'? <Image style={imageX.image} source={ require('../image/p/F.png') } />: 
+			value == 'G'? <Image style={imageX.image} source={ require('../image/p/G.png') } />: 
+			value == 'H'? <Image style={imageX.image} source={ require('../image/p/H.png') } />: 
+			value == 'I'? <Image style={imageX.image} source={ require('../image/p/I.png') } />: 
 			undefined
     	}
 	
